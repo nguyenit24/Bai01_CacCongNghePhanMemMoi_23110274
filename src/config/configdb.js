67ca8831e3ae5310fec624459301c04a7acfd59a
Duy Nguyen Huynh <nguyenhuynh.463459@gmail.com>
2026-05-04 13:22:50 +0700

    first commit

---
+++ src/config/configdb.js
@@ -0,0 +1,19 @@
+import { Sequelize } from 'sequelize';
+
+// Khởi tạo instance Sequelize với các tham số từ config
+const sequelize = new Sequelize('node_fulltask', 'root', '123', {
+    host: 'localhost',
+    dialect: 'mysql',
+    logging: false
+});
+
+let connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('>>> Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+module.exports = connectDB;
---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/ViewEngine";
+import initWebRoute from "./route/web";
+import connectDB from "./config/configdb";
+
+require('dotenv').config();
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+viewEngine(app);
+initWebRoute(app);
+connectDB();
+
+let port = process.env.PORT || 6969;
+
+app.listen(port, () => {
+    console.log("Backend Nodejs is running on the port : " + port);
+});
